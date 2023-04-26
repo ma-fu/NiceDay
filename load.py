ea30341c6394fe45449903082f028e7b140ee814
fuma <magithubfu@gmail.com>
2023-04-26 02:40:35 +0000

    sort code a bit

--- load.py
+++ load.py
@@ -1,9 +1,33 @@
-from sql import Model
+from sql import Asker
+
+def tbl_info(tbl_id):
+	tbl_nam = model.tbl_name(tbl_id)
+	tbl_df = model.tbl_df(tbl_nam)
+	model.disp_sql(tbl_df)
+	print("\tinsert:i")
+	return tbl_nam, tbl_df
+
+def case_insert(tbl_df):
+	data = model.req_ins_data(tbl_df)
+	data["tbl"] = tbl_nam
+	clms,esc,vls,tbl = data 
+	q = "insert into %s(%s) values (%s)" %  (data[tbl],data[clms],data[esc])
+	print(data[vls])
+	return data
+	
+def confirm_ins(data):
+	yn = input("Would you like to save?(y/n)")
+	if yn =="y":
+		print("Saving")
+		model.insert_data(data)
+	else:
+		print("Bye")
+	
 
 ask_file  = "db name:"
 db_name = input(ask_file)
 
-model = Model(db_name)
+model = Asker(db_name)
 
 if model.isSQLite3():
 	print("Successfuly connect %s!" % (db_name))
@@ -11,41 +35,32 @@ else:
 	print("%s is not SQLite3" % (db_name))
 	exit()
 
-tbls_df = model.df_tbls()
+tbls_df = model.tbls_df()
 
-# Display table data 
+# Display table data or some operations 
 tbl_ids = list(map(str, tbls_df.index))
 order = None
 tbl_id = None
 while order !="q":
+	# First time or typed 't'
 	if order is None or order=="t":
 		model.disp_sql(tbls_df,idx=True)
-	
+
+	# Insert mode	
 	if order=="i":
 		order = tbl_id
-		data = model.req_ins_data(tbl_df)
-		data["tbl"] = tbl_nam
-		clms,esc,vls,tbl = data 
-		q = "insert into %s(%s) values (%s)" %  (data[tbl],data[clms],data[esc])
-		print(q)
-		print(data[vls])
-		yn = input("Would you like to save?(y/n)")
-		if yn =="y":
-			print("saving")
-			model.insert_data(data)
-		else:
-			print("bye")
+		data = case_insert(tbl_df)
+		confirm_ins(data)
 
+	# Usr value not in tble menu
 	if order not in tbl_ids:
 		tbl_id = model.req_tbl_id(tbl_ids) 
 	else:
 		tbl_id = order
-
+	
+	# Prompt which tbl
 	if tbl_id != "q":
-		tbl_nam = model.tbl_from_id(tbl_id)
-		tbl_df = model.df_from_tbl(tbl_nam)
-		model.disp_sql(tbl_df)
-		print("\tinsert:i")
+		tbl_nam,tbl_df = tbl_info(tbl_id)
 		
 	print("\ttbls:t\texit:q\ttbl_id:")
 	order = input("Continue?")
--- sql.py
+++ sql.py
@@ -3,30 +3,58 @@ from tabulate import tabulate as tabu
 from os.path import isfile, getsize
 
 class Model:
-	def __init__(self, name):
-		self.name = name
+	def __init__(self, db):
+		self.db = db
 
 	def isSQLite3(self):
-		if not isfile(self.name):
+		if not isfile(self.db):
 			return False
-		if getsize(self.name) < 100:
+		if getsize(self.db) < 100:
 			return False
-		with open(self.name,"rb") as fd:
+		with open(self.db,"rb") as fd:
 			header = fd.read(100)[:16]
 		b16 = b"SQLite format 3\x00"
+
 		return header == b16
 
-	def df_tbls(self):
+	def tbls_df(self):
 		tbls_query = "select name from sqlite_master where type='table'"
-		with sqlite3.connect(self.name) as db:
+		with sqlite3.connect(self.db) as db:
 			tbls_df = pd.read_sql(tbls_query,db)
 			self.tbls_df = tbls_df
-			return tbls_df
 
-	def df_from_tbl(self,tbl_name):
-		with sqlite3.connect(self.name) as db:
+		return tbls_df
+
+	def tbl_df(self,tbl_name):
+		with sqlite3.connect(self.db) as db:
 			sel_all = "select * from %s" % (tbl_name)
-			return pd.read_sql(sel_all,db)
+		return pd.read_sql(sel_all,db)
+
+
+	def insert_data(self,data):
+		clms,esc,vls,tbl = data
+		q = "insert into %s(%s) values (%s)" %  (data[tbl],data[clms],data[esc])
+		with sqlite3.connect(self.db) as db:
+			cur = db.cursor()
+			cur.execute(q,data[vls])	
+			db.commit()
+		time.sleep(1)
+
+	def tbl_name(self,order):
+		order = int(order)
+		return self.tbls_df.iloc[order][0]
+
+	def clms_tbl_nam(self,tbl_nam):
+		q = "pragma table_info(%s)" % tbl_nam
+		with sqlite3.connect(self.db) as db:
+			return pd.read_sql(q,db).name
+
+	def disp_sql(self,df,idx=False):
+		clms = df.columns.values
+		disp = tabu(df,headers=clms,tablefmt="psql",showindex=idx)
+		print(disp)
+
+class Asker(Model):
 
 	def req_tbl_id(self,idxs):
 		order = None
@@ -43,7 +71,7 @@ class Model:
 			vl = "" 
 			if df[cl].dtypes=="int64":
 				print("\tNumber\ts:skip")
-				while vl.isnumeric() is False:
+				while not vl.isnumeric():
 					vl = input("%s:" % cl)
 					if vl=="s":
 						break
@@ -58,36 +86,9 @@ class Model:
 		print(esc)
 		return {"clms":clms_str[:-1],"esc":esc[:-1],"vls":usr_vl}
 
-	def insert_data(self,data):
-		clms,esc,vls,tbl = data
-		q = "insert into %s(%s) values (%s)" %  (data[tbl],data[clms],data[esc])
-		with sqlite3.connect(self.name) as db:
-			cur = db.cursor()
-			cur.execute(q,data[vls])	
-			db.commit()
-		time.sleep(1)
-
-	def tbl_from_id(self,order):
-		order = int(order)
-		return self.tbls_df.iloc[order][0]
-
-	def clms_tbl_nam(self,tbl_nam):
-		q = "pragma table_info(%s)" % tbl_nam
-		with sqlite3.connect(self.name) as db:
-			return pd.read_sql(q,db).name
-
-	def disp_sql(self,df,idx=False):
-		clms = df.columns.values
-		disp = tabu(df,headers=clms,tablefmt="psql",showindex=idx)
-		print(disp)
 
 		
 if __name__ == "__main__":
-	test_db = "2023.db"
-	md = Model(test_db)
-	md.disp_sql(md.df_tbls(),idx=True)
-	tbl_str = md.tbl_from_id(14)
-	print(md.clms_tbl_nam(tbl_str))
-	
+	pass
 	
 	
